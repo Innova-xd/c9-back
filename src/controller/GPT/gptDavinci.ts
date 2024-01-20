23134080229cf79f357938e13f84a5d287817b8c
Daniel Atik <danielatik@users.noreply.github.com>
2024-01-19 22:09:55 -0300

    Revert "Update gptDavinci.ts"
    This reverts commit 54055bd2a6d1f84cb9cc0814b329ee921d49388a.

--- src/controller/GPT/gptDavinci.ts
+++ src/controller/GPT/gptDavinci.ts
@@ -12,7 +12,7 @@ gptDavinciRouter.post('/davinci',  async  (req: Request, res: Response)  => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            model : "gpt-3.5-turbo",
+            model : "text-davinci-003",
             prompt: req.body.prompt,
             max_tokens: req.body.max_tokens,
             n: req.body.n,
